cc8ae6db6bbeadae6b2b58dc64e5b7aca55c9f4b
Ismail Ceylan <ismaillceylan@gmail.com>
2023-10-21 10:23:59 +0300

    refactor: dedicated methods

--- src/utils/once.js
+++ src/utils/once.js
@@ -1,29 +1,46 @@
 export default function once()
 {
 	const stack = [];
+	let isMounted = false;
 
-	return function snapshot( val, old, bindings )
+	function mount()
+	{
+		if( isMounted )
+		{
+			return;
+		}
+
+		queueMicrotask( handleStack );
+		isMounted = true;
+	}
+	
+	function queue( val, old, bindings )
 	{
 		stack.push([ val, old, bindings ]);
 
-		if( stack.length === 1 )
+		if( ! isMounted )
 		{
-			queueMicrotask(() =>
-			{
-				const first = stack.shift();
-				const last = stack.pop() || first;
-
-				const bindings = Array.isArray( first[ 2 ])
-					?   first[ 2 ]
-					: [ first[ 2 ]];
-
-				for( const cb of bindings )
-				{
-					cb && cb( last[ 0 ], first[ 1 ]);
-				}
-
-				stack.length = 0;
-			});
+			mount();
 		}
 	}
+
+	function handleStack()
+	{
+		const first = stack.shift();
+		const last = stack.pop() || first;
+
+		const bindings = Array.isArray( first[ 2 ])
+			?   first[ 2 ]
+			: [ first[ 2 ]];
+
+		for( const cb of bindings )
+		{
+			cb && cb( last[ 0 ], first[ 1 ]);
+		}
+
+		stack.length = 0;
+		isMounted = false;
+	}
+
+	return queue;
 }
