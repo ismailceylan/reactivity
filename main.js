03bae373e51282a8aec06749755cdf9f919d331c
Ismail Ceylan <ismaillceylan@gmail.com>
2023-10-23 15:48:54 +0300

    refactor: computed tests

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { ref, reactive, watchEffect, unref, computed } from "./src/index.js";
+import { ref, reactive, watchEffect, unref, computed, watch } from "./src/index.js";
 
 const button = document.querySelector( "#increase" );
 const delLatest = document.querySelector( "#del-latest" );
@@ -8,16 +8,32 @@ const count = document.querySelector( "#count" );
 const content = document.querySelector( "#content" );
 const currentId = document.querySelector( "#curr-id" );
 const price = document.querySelector( "#price" );
+const priceRow = document.querySelector( "#price-row" );
 const tax = document.querySelector( "#tax" );
+const beforeTax = document.querySelector( "#before-tax" );
 
 const id = ref( 0 );
 const books = reactive([]);
+
 const totalPrice = computed(() =>
 	books
 		.map( book => book.price )
 		.reduce(( a, b ) => a + b, 0 )
 );
-const taxPrice = computed(() => totalPrice.value * .2 );
+
+const taxPrice = computed(() =>
+	Math.round( totalPrice.value * .2 )
+);
+
+watch( totalPrice, price =>
+
+	priceRow.setAttribute(
+		"style",
+		price > 1000
+			? "color:red; font-weight:bold"
+			: ""
+	)
+);
 
 watchEffect(() =>
 {
@@ -26,6 +42,7 @@ watchEffect(() =>
 	currentId.innerText = unref( id );
 	price.innerText = totalPrice.value;
 	tax.innerText = taxPrice.value;
+	beforeTax.innerText = totalPrice.value - taxPrice.value;
 });
 
 button.addEventListener( "click", createBook );
