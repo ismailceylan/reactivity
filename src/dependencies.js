5f64bc96eaacaceeb52294815da075b79a9bcb28
Ismail Ceylan <ismaillceylan@gmail.com>
2023-10-23 13:57:52 +0300

    refactor: nested dependencies capturing

--- src/dependencies.js
+++ src/dependencies.js
@@ -2,11 +2,29 @@ export const deps = new Set;
 
 export default function dependencies( effects )
 {
-	// we should make sure deps stack is empty
+	// we should make sure backup current dependencies
+	// to open a clean room for this capturing session
+	const before = [ ...deps ];
+
+	// room cleaning
 	deps.clear();
 
-	return {
+	const data =
+	{
+		// reactive objects in this callback are gonna
+		// register themselves into the deps set
 		returnValue: effects(),
+		// we collected refs
 		refs: [ ...deps ]
 	}
+
+	// we should make sure clean our mess
+	deps.clear();
+
+	// restore previous session
+	before.forEach( item =>
+		deps.add( item )
+	);
+
+	return data;
 }
