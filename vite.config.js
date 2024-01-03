1498ada25ab38984868de6f909a3d954ef5694db
Ismail Ceylan <ismaillceylan@gmail.com>
2024-01-03 23:24:59 +0300

    build: install vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(
+{
+	plugins:
+	[
+	],
+
+	resolve:
+	{
+		alias:
+		{
+			"@": fileURLToPath(
+				new URL( "./src", import.meta.url )
+			)
+		}
+	},
+
+	build:
+	{
+		cssCodeSplit: false
+	}
+});
